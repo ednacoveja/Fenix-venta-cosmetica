137465e73484a120e77f405d0e957309de5ba9c4
ednacoveja <ovejerocande@gmail.com>
2023-04-12 19:36:16 -0300

    landing y limpieza

--- Front/src/components/Landing.jsx
+++ Front/src/components/Landing.jsx
@@ -1,5 +1,33 @@
+import './Landing.css'
+import React from 'react'
+import { useNavigate } from "react-router-dom";
+
+
 export const Landing = () => {
-    return(
-        <div>Landing</div>
+
+    const navigate = useNavigate();
+
+    async function ALaHome(e) {
+        e.preventDefault();
+        navigate("/home");
+    }
+
+    return (
+        <div className='landing'>
+            <div className='laCont'>
+                <div className='landArribaCont'>
+                    <div className='logo'>Emprendimiento Artesanal</div>
+                    <div >
+                        <button className='btnCreCu' onClick={(e) => ALaHome(e)}>Tienda Online</button>
+                    </div>
+                </div>
+                <div className='lan1Cont'>
+                    <div className='lan1Left'>
+                     
+                    </div>
+                </div>
+            </div>
+        </div>
+
     )
-};
+}
